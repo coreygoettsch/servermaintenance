78494831042885b63e4702c9b0d22d3fd94893ae
nicholas biddle <coreygoettsch@gmail.com>
2017-01-27 16:59:36 -0500

    Added an incomplete version of a script that uses GNU Dialog to perform administrative tasks.  The first one on the script is backing up /var/www/

---
+++ adminmenu.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+# adminmenu.sh - This is a shell script designed to present a dialog menu of
+# administration tasks, simplifying them.
+
+#Temporary storage  file for menu  options chosen by user
+INPUT=/tmp/admin.sh.$$
+
+#Temporary storage file for displaying output
+OUTPUT=/tmp/admin.sh.$$
+
+# Trap and delete temporary files
+trap "rm $INPUT; rm $OUTPUT; exit" SIGHUP SIGINT SIGTERM
+
+# This function displays output through msgbox
+function display_output(){
+	local h=${1-10} #Default box height 10
+	local w=${2-41} #Default box width 41
+	local t=${3-Output} #Box title
+	dialog --backtitle "Administration Tasks" --title "${t}" --clear --msgbox "$(<$OUTPUT)" ${h} ${w}  
+}  
+
+#This function displays 
+
+function show_backup(){
+	tar -cvfz websitebackup.tar.gz /var/www/>$OUTPUT 
+	display_output 10 30 "Backup"
+}
+
+# set infinite loop
+while true 
+do
+
+# The main menu
+dialog --clear --help-button --backtitle "System Administration Help Tool" \
+--title "Main Menu" \
+--menu "You can use the Up and Down arrow keys, the first \n\
+letter of the choice as a hot key, or the \n\
+number keys 1-9 to choose an option.\n\
+Choose the TASK" 15 50 2 \
+Backup "Backs up the web directory" \
+Exit "Exits to the shell" 2>"${INPUT}"
+
+menuitem=$(<"${INPUT}") 
+
+#make decision
+case $menuitem in
+	Backup) run backup;;
+	Exit) echo "Bye"; break;;
+
+esac
+
+done
