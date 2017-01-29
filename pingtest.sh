be2dea33e8dc36e16a88d84242b8de0049bc51b6
nicholas biddle <coreygoettsch@gmail.com>
2017-01-29 10:25:21 -0500

    Added arguments to the pingtest script to simplify execution.

--- pingtest.sh
+++ pingtest.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-DATE=`date`
-URL=www.website.com # Insert the server ip address or domain name here
-EMAIL=root@localhost.com #Insert the admin's email address here. 
+# Argument 1 ($1) is the server's url, and Argument 2 ($2)is the email address to which an error message should be sent
+# the syntax of the this shell script ought to be "./pingtest.sh URL Email@address.com
 
-ping -c 3 $URL > /dev/null
+DATE=`date`
+
+ping -c 3 $1 > /dev/null
 
 if [ $? -eq 0 ]
 	then 
@@ -12,7 +13,7 @@ if [ $? -eq 0 ]
 
 elif [ $? -eq 1 ] 
 	then
-		echo "Ping Test Failed on $DATE!" >> $HOME/pingtest.log && echo "Ping Test Failed on $DATE!" | mail -s "Ping Test Failure!" $EMAIL
+		echo "Ping Test Failed on $DATE!" >> $HOME/pingtest.log && echo "Ping Test Failed on $DATE!" | mail -s "Ping Test Failure!" $2
 
 fi 
 
