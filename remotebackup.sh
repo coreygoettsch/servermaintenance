b2c9aa1a4ab0a2ba42c412239ec6ed58e9ed6fd0
nicholas biddle <coreygoettsch@gmail.com>
2017-02-14 11:52:55 -0500

    Deleted unnecessary comments.

--- remotebackup.sh
+++ remotebackup.sh
@@ -9,13 +9,8 @@
 # $2 ip address of remote server
 # $3 remote directory to be backed up (e.g. /var/www/bob)
 # $4 is the name of the .tar.gz backup (e.g. bobbackup) 
-# End of arguments 
 
-# Global variables
 DATE=`date`
-# Global variables end
-
-# Script begin
 
 echo "Remote Backup Beginning at $DATE"
 echo "Connecting to remote server."
@@ -23,4 +18,3 @@ ssh  $1@$2 "cd $3 && tar cvfz $HOME/$4.tar.gz ."
 scp $1@$2:/home/$1/$4.tar.gz .
 echo "Backup Completed."
 
-# Script complete 
